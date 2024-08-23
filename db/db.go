7316e7a41b237bdd09f63ce2475f2bde92cb0ccd
Juan Arias <juanmanuel0560@gmail.com>
2024-08-23 13:22:26 -0500

    endpoint to signup and the hash for passwords has been added successfully

--- db/db.go
+++ db/db.go
@@ -21,6 +21,18 @@ func InitDB() {
 }
 
 func createdTables() {
+	createUsersTable := `
+	CREATE TABLE IF NOT EXISTS users(
+	 id INTEGER PRIMARY KEY AUTOINCREMENT,
+	 email TEXT NOT NULL UNIQUE,
+	 password TEXT NOT NULL
+	)	
+	`
+	_, err := DB.Exec(createUsersTable)
+	if err != nil {
+		panic("Could not created users table.")
+	}
+
 	createEventsTable := `
 	CREATE TABLE IF NOT EXISTS events(
 	 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -28,11 +40,12 @@ func createdTables() {
 	 description TEXT NOT NULL,
 	 location TEXT NOT NULL,
 	 dateTime DATETIME NOT NULL,
-	 user_id INTEGER	
+	 user_id INTEGER,
+	 FOREIGN KEY(user_id) REFERENCES user(id)
 	)	
 	`
 
-	_, err := DB.Exec(createEventsTable)
+	_, err = DB.Exec(createEventsTable)
 
 	if err != nil {
 		panic("Could not created events table.")
