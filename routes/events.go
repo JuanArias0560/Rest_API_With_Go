6579fe569f6255929da006800255d7debf5450a2
Juan Arias <juanmanuel0560@gmail.com>
2024-08-23 12:31:13 -0500

    The endpoint to delete a event has been added

--- routes/events.go
+++ routes/events.go
@@ -79,3 +79,24 @@ func updateEvent(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{"message": "Event Updated successfully"})
 
 }
+
+func deleteEvent(context *gin.Context) {
+	enventID, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+	event, err := models.GetEventById(enventID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event."})
+		return
+	}
+	event.DELETE()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not deleted the event."})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Event deleted successfully"})
+
+}
